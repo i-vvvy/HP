e50827681313407c8be65fbc1f91664b975457fc
i_vvvy <ivvvy.e@gmail.com>
2022-12-14 14:43:53 +0900

    스타일 및 애니메이션 적용

--- js/script_main.js
+++ js/script_main.js
@@ -9,21 +9,31 @@ $(function () {
       'fourthPage',
       'fifthPage',
       'sixthPage',
-      'seventhPage',
     ],
-    menu: '.rightNav',
     afterLoad: function (anchorLink, index) {
-      if (index == 2) {
+      if (index == 4) {
         $('.count').counterUp({ delay: 10, time: 550 });
-      }
-      if (index == 3) {
-        $('.count2').counterUp({ delay: 10, time: 550 });
       }
     },
   });
 });
 
-//2.비디오 Controls 숨기기
+//2.GNB
+$(function () {
+  setGnb();
+  function setGnb() {
+    $('.innerHeader .gnb > li > a ').on('mouseenter focus', function () {
+      $('.innerHeader ').addClass('on');
+      $('.innerHeader .gnb > li > .inner').addClass('on');
+    });
+    $('header').on('mouseleave', function () {
+      $('.innerHeader ').removeClass('on');
+      $('.innerHeader .gnb > li > .inner').removeClass('on');
+    });
+  }
+});
+
+//3.section1: 비디오 Controls 숨기기
 let vids = $('video');
 $.each(vids, function () {
   this.controls = false;
@@ -39,38 +49,54 @@ $('video').click(function () {
   }
 });
 
-//3.section3 이미지 슬라이드
+//4.section3: 이미지 슬라이드
 $(function () {
   $('.visual').slick({
     arrows: false,
-    dots: true,
     fade: true,
     autoplay: true,
     pauseOnHover: false,
     pauseOnFocus: false,
     autoplaySpeed: 3000,
-    customPaging: function (slider, i) {
-      let tit = $(slider.$slides[i]).find('.dot').html();
-      return '<div class="pager-tit" class= "+ i +">' + tit + '</div>';
-    },
+    dot: false,
   });
 });
 
-//4.section3 이미지 슬라이드 - 넓이, 높이
-
-$(function () {
-  let winW = $(window).width();
-  let winH = $(window).height();
-  list = $('.visual .list');
-  list.css({ width: winW + 'px', height: winH + 'px' });
-});
-
-//5.section5 - products 슬라이드
+//5.section5: products 슬라이드
 
 $(function () {
   $('.product').slick({
     slidesToShow: 3,
     slidesToScroll: 1,
-    // dots: true,
+    autoplaySpeed: 3000,
   });
+});
+
+//6.section: width, height 설정
+$(function () {
+  var winW = $(window).width();
+  var winH = $(window).height();
+  list = $('#section1 .container');
+  list.css({ width: winW + 'px', height: winH + 'px' });
+});
+
+$(function () {
+  var winW = $(window).width();
+  var winH = $(window).height();
+  list = $('#section3 .visual .list');
+  list.css({ width: winW + 'px', height: winH + 'px' });
+});
+
+$(function () {
+  var winW = $(window).width();
+  var winH = $(window).height();
+  list = $('#section5 .container');
+  list.css({ width: winW + 'px', height: winH + 'px' });
+});
+
+$(function () {
+  var winW = $(window).width();
+  var winH = $(window).height();
+  list = $('#section6 .container');
+  list.css({ width: winW + 'px', height: winH + 'px' });
 });
